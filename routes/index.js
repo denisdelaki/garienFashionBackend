3bb98116b4a296c9d323433a6d61d84f0a15b8f2
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-07-27 14:32:22 +0300

    modified prodcts route rendereed pug insteaded

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Garien Fashion API', status: 'running' });
 });
 
-// Import the products router
-var productsRouter = require('./products');
-// Use the products router for routes starting with /products
-router.use('/products', productsRouter);
+
 
 
 
