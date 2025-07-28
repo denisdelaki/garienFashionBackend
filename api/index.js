cbda92f1ece7fbfd854e0b0c4311f43a68cc1a26
Denis kipyegon <denis.kipyegon@equitybank.co.ke>
2025-07-28 11:07:51 +0300

    reemoved index durration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const createError = require('http-errors');
+const cors = require('cors');
 
 const indexRouter = require('../routes/index');
 const usersRouter = require('../routes/users');
@@ -19,8 +23,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
+const corsOptions = {
+  origin: [
+    'http://localhost:4200',
+    'https://garien-fashion.vercel.app'
+  ],
+  credentials: true,
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = serverless(app);
